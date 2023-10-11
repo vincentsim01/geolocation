364185002ba02bd2128d3714bd1c0841456452d2
Vincent Kiathadi <vincentkiathadi@gmail.com>
2023-10-11 21:54:06 +0800

    geo

---
+++ script.js
@@ -0,0 +1,13 @@
+let theLocation=document.getElementById("theLocation");
+
+if (navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(tunjukkanPosisi);
+}else{
+    theLocation.innerHTML="No Geo Location available";
+}
+
+function tunjukkanPosisi(sangPosisi){
+    theLocation.innerHTML= "latitude is:"+sangPosisi.coords.latitude+"<br>"+
+    "longitude is:"+sangPosisi.coords.longitude;
+
+}
